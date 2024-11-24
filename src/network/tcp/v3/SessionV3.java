dba65f2bee2f0a593317f933ac24300b424b0296
cheomuk <pkw9815@naver.com>
2024-11-24 09:01:25 +0900

    Commit 241124

---
+++ src/network/tcp/v3/SessionV3.java
@@ -0,0 +1,49 @@
+package network.tcp.v3;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import static util.MyLogger.log;
+
+public class SessionV3 implements Runnable {
+
+    private final Socket socket;
+
+    public SessionV3(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+            while (true) {
+                // 클라이언트로부터 문자 받기
+                String received = input.readUTF();
+                log("client -> server: " + received);
+
+                if (received.equals("exit")) {
+                    break;
+                }
+
+                // 클라이언트에게 문자 보내기
+                String toSend = received + " World!";
+                output.writeUTF(toSend);
+                log("client <- server: " + toSend);
+            }
+
+            // 자원 정리
+            log("연결 종료: " + socket);
+            input.close();  // 위에서 Exception 이 발생하면 자원 정리 코드는 실행되지 못하고 탈출하게 된다!
+            output.close(); // 매우 심각한 문제가 발생하게 됨
+            socket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
